3cadecca97e936ed10528e41dcfa2c47312b0a71
abhavedula <abhaved@seas.upenn.edu>
2016-01-23 23:10:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
   <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
 
   <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="magnific-popup/magnific-popup.css">
+  <link rel="stylesheet" href="magnific-popup.css">
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-  <script src="magnific-popup/jquery.magnific-popup.js"></script>
+  <script src="jquery.magnific-popup.js"></script>
 
 </head>
 <body>
@@ -133,7 +133,54 @@ $conn->close();
   </div>
 </div>
 
+<script>$(document).ready(function() {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
 
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+});</script>
+
+<!-- link that opens popup -->
+<a class="popup-with-form" href="#test-form">Open form</a>
+
+<!-- form itself -->
+<form id="test-form" class="mfp-hide white-popup-block">
+	<h1>Form</h1>
+	<fieldset style="border:0;">
+		<p>Lightbox has an option to automatically focus on the first input. It's strongly recommended to use <code>inline</code> popup type for lightboxes with form instead of <code>ajax</code> (to keep entered data if the user accidentally refreshed the page).</p>
+		<ol>
+			<li>
+				<label for="name">Name</label>
+				<input id="name" name="name" type="text" placeholder="Name" required="">
+			</li>
+			<li>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="example@domain.com" required="">
+			</li>
+			<li>
+				<label for="phone">Phone</label>
+				<input id="phone" name="phone" type="tel" placeholder="Eg. +447500000000" required="">
+			</li>
+			<li>
+				<label for="textarea">Textarea</label><br>
+				<textarea id="textarea">Try to resize me to see how popup CSS-based resizing works.</textarea>
+			</li>
+		</ol>
+	</fieldset>
+</form>
 
 <div id="restaurant" class="content-block reservations-block">
   <div class="reservations-block-inner container">

2711f343b03891a2176ac5996ed35033012effea
Ming Zhang <ming-zhang@users.noreply.github.com>
2016-01-23 19:41:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
             If you would like to help homeless shelters, you can check in the amount of food left in your restaurant here
           </p>
         </div>
-        <?php if ($_POST) {echo '
+        <?php if ($_POST) { echo '<p> Thank you!</p>'; } else { echo '
         <form action="/" method="post" class="reservations-form">
           <fieldset>
             <legend class="sr">Contact Us</legend>
@@ -170,7 +170,7 @@
             </div>
           </fieldset>
         </form>
-        ';} else { echo '<p> Thank you!</p>'; }?>
+        ';}?>
       </div>
     </div>
   </div>

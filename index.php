af811748ca056e1a0ef07efc0c1eb37308cae49e
Ming Zhang <ming-zhang@users.noreply.github.com>
2016-01-24 17:10:37 -0500

    content description

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Foodship</title>
 
-  <meta name="description" content="">
+  <meta name="description" content="Foodship connects homeless shelter to restaurants donating food.">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="shortcut icon" href="img/favicon.png">

ddfc0584da2ad183e578b7900e9d655a241d0c80
Ming Zhang <ming-zhang@users.noreply.github.com>
2016-01-24 22:34:27 -0500

    add ribbon

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 
 
 <div class="banner">
+  <div class="ribbon"><span>POPULAR</span></div>
   <div class="header">
     <div class="header-inner container clear">
       <a class="logo" href="#"><span class="sr">Lambda Logo</span></a>

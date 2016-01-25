81ccba0e81440156882d2e098007a4d8c192ca61
Ming Zhang <ming-zhang@users.noreply.github.com>
2016-01-24 22:44:30 -0500

    ribbon hyperlink

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
 
 <div class="banner">
-  <div class="ribbon"><span>PENNAPPS XIII</span></div>
+  <div class="ribbon"><a href="http://devpost.com/software/foodship"></a><span>PENNAPPS XIII</span></a></div>
   <div class="header">
     <div class="header-inner container clear">
       <a class="logo" href="#"><span class="sr">Lambda Logo</span></a>
